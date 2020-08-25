be51038a18627d57aeaae0dc80f9aa1139a68d94
Jorge Epuñan <juanbrujo@gmail.com>
2020-08-25 12:24:00 -0400

    preserve old versions

---
+++ vue/dist/precache-manifest.151d7b72ecb687f2badb4104005d605e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b5560bf6cfa8d6562ff",
+    "url": "/css/app.126ae811.css"
+  },
+  {
+    "revision": "d5b1e9e58d9cdc47019041b939506666",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b5560bf6cfa8d6562ff",
+    "url": "/js/app.b1a35be7.js"
+  },
+  {
+    "revision": "e559579e086b9d59e203",
+    "url": "/js/chunk-vendors.6736a5d8.js"
+  },
+  {
+    "revision": "9dfc1b388c32a27f973aec9153c5616b",
+    "url": "/kushki/index.html"
+  },
+  {
+    "revision": "95409aa88af05f2796c5a65380e74d61",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
